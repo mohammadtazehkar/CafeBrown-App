186e9495f87e69ca80185b6c90bfc6ed4b9ded06
mahdiarat1993 <mahdi.shokri1993@gmail.com>
2024-01-10 01:59:26 -0800

    firs push

--- app/src/main/java/com/example/cafebrown/MainActivity.kt
+++ app/src/main/java/com/example/cafebrown/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface {
                     AppNavHost()
+
                 }
             }
         }
